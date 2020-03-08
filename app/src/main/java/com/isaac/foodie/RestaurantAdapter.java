a693e850923523d8793fae6cda4b2c536ac68521
TritonX <isaaclim96@gmail.com>
2020-03-08 23:36:08 +0800

    Recyclerview onClick test work

--- app/src/main/java/com/isaac/foodie/RestaurantAdapter.java
+++ app/src/main/java/com/isaac/foodie/RestaurantAdapter.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -20,17 +19,14 @@ import java.util.List;
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder> {
 
     Context mContext;
-    ArrayList<RestaurantDetails> mRestaurant;
-    //List<RestaurantDetails> mData;
+    ArrayList<RestaurantDetails> mRestaurantDetailsArrayList;
+    private OnRestaurantListener mOnRestaurantListener;
 
-//    public RestaurantAdapter(Context context, List<RestaurantDetails> data) {
-//        mContext = context;
-//        mData = data;
-//    }
+
 
     public RestaurantAdapter(Context c, ArrayList<RestaurantDetails> r){
         mContext = c;
-        mRestaurant=r;
+        mRestaurantDetailsArrayList=r;
     }
 
     @NonNull
@@ -48,22 +44,23 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Re
     public void onBindViewHolder(@NonNull RestaurantViewHolder restaurantViewHolder, int position) {
             //bind data here
 
-        restaurantViewHolder.restaurant_Name.setText(mRestaurant.get(position).getName());
-        restaurantViewHolder.restaurant_Address.setText(mRestaurant.get(position).getAddress());
-        restaurantViewHolder.restaurant_Location.setText(mRestaurant.get(position).getLocation());
-        restaurantViewHolder.restaurant_Category.setText(mRestaurant.get(position).getCategory());
+        restaurantViewHolder.restaurant_Name.setText(mRestaurantDetailsArrayList.get(position).getName());
+        restaurantViewHolder.restaurant_Address.setText(mRestaurantDetailsArrayList.get(position).getAddress());
+        restaurantViewHolder.restaurant_Location.setText(mRestaurantDetailsArrayList.get(position).getLocation());
+        restaurantViewHolder.restaurant_Category.setText(mRestaurantDetailsArrayList.get(position).getCategory());
     }
 
     @Override
     public int getItemCount() {
-        return mRestaurant.size();
+        return mRestaurantDetailsArrayList.size();
     }
 
-    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView restaurant_Longitude,restaurant_Latitude;
         TextView restaurant_Name,restaurant_Address,restaurant_Category,restaurant_Location;
 
+        OnRestaurantListener onRestaurantListener;
 
         public RestaurantViewHolder(@NonNull View itemView){
             super(itemView);
@@ -72,7 +69,19 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Re
             restaurant_Address = itemView.findViewById(R.id.restaurant_Address);
             restaurant_Category = itemView.findViewById(R.id.restaurant_Category);
             restaurant_Location = itemView.findViewById(R.id.restaurant_Location);
+
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View view) {
+            onRestaurantListener.onRestaurantClick(getAdapterPosition());
+
+        }
+    }
+
+    public interface OnRestaurantListener{
+        void onRestaurantClick(int position);
     }
 
 
