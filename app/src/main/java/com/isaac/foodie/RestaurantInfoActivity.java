e800b208817aabba5592522b11380eb0be7985e5
TritonX96 <isaaclim96@gmail.com>
2020-03-16 11:31:41 +0800

    Display issue fix due to arrangement. Will now focus on adding images and location button

--- app/src/main/java/com/isaac/foodie/RestaurantInfoActivity.java
+++ app/src/main/java/com/isaac/foodie/RestaurantInfoActivity.java
@@ -4,22 +4,44 @@ package com.isaac.foodie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
-public class RestaurantInfoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RestaurantInfoActivity extends AppCompatActivity{
 
     TextView iRestaurantName, iRestaurantCategory, iRestaurantAddress, iRestaurantLocation;
+    private Toolbar toolbar;
+    private static final String TAG = RestaurantInfoActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restaurant_details);
+        Log.d(TAG, "onCreate: started.");
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+   
+
 
         //Action bar
         //ActionBar actionBar = getSupportActionBar();
@@ -30,7 +52,6 @@ public class RestaurantInfoActivity extends AppCompatActivity {
         //actionBar.setDisplayShowHomeEnabled(true);
 
         iRestaurantName = findViewById(R.id.restaurantName);
-        //iRestaurantAddress = findViewById(R.id.restaurantAddress);
         iRestaurantCategory = findViewById(R.id.restaurantCategory);
         iRestaurantLocation = findViewById(R.id.restaurantLocation);
         //iRestaurantAddress = findViewById(R.id.restaurantAddress);
@@ -40,8 +61,11 @@ public class RestaurantInfoActivity extends AppCompatActivity {
 
         //Receive Data
         String name = i.getExtras().getString("NAME_KEY");
+        Log.d(TAG,"The name is " + name);
         String category = i.getExtras().getString("CATEGORY_KEY");
+        //Log.i(category, "This is category");
         String location = i.getExtras().getString("LOCATION_KEY");
+        //Log.i(location, "This is location");
 
         //Bind data
         iRestaurantName.setText(name);
@@ -51,6 +75,7 @@ public class RestaurantInfoActivity extends AppCompatActivity {
 
 
     }
+
 
     //handle onBackPressed to previous activity
     @Override
